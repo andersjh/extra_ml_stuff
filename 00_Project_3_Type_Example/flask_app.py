aa99e7ed41407c1032d564a36a016f0e81d8f9cd
andersjh <andersjh120@gmail.com>
2021-11-05 21:59:31 -0500

    minor cleanup

--- 00_Project_3_Type_Example/flask_app.py
+++ 00_Project_3_Type_Example/flask_app.py
@@ -7,10 +7,12 @@ import pickle
 # Create an instance of Flask
 app = Flask(__name__)
 
+# Now let's load our model ( note, we just use it )
 with open('best_xgb_model.pickle', "rb") as f:
     model = pickle.load(f)
 
 feature_names = model.get_booster().feature_names
+print(f"\nOur model has the following features:\n{feature_names}\n")
 
 # Route to render index.html template using data from Mongo
 @app.route("/", methods=["GET", "POST"])
@@ -25,7 +27,10 @@ def home():
 
         # data must be converted to df with matching feature names before predict
         data = pd.DataFrame(np.array([[recency, frequency, monetary, time]]), columns=feature_names)
+        print("About to predict for:")
+        print(data)
         result = model.predict(data)
+        print(f"\nThe model predicted{result}!")
         if result == 1:
             output_message = "Nice, you will donate soon, thank you ^_^"
         else:
